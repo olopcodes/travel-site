0272ff5dc681a5cecb0f24c98235dbc2a11e3e62
olopcodes <olopcodes@gmail.com>
2020-03-14 21:07:18 -0400

    finished the nav menu for the site header

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,5 +1,22 @@
 import '../styles/styles.css';
+import MobileMenu from './modules/MobileMenu';
+
+const mobileMenu = new MobileMenu();
 
 if(module.hot) {
     module.hot.accept()
 }
+
+
+// inheritance
+// class Adult extends Person {
+//     payTaxes () {
+//         console.log(`${this.name} has to pay taxes now!`)
+//     }
+// }
+// const bob = new Person('Bob', 'purple');
+// bob.greet();
+
+// const jan = new Adult('Jan', 'orange');
+// jan.greet();
+// jan.payTaxes();
