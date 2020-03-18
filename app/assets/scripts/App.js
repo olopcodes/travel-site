c4a7356d5658c80c1cf186bbe28da388a35eae3d
olopcodes <olopcodes@gmail.com>
2020-03-18 15:49:41 -0400

    finished the modal, with js capabilities

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -2,8 +2,11 @@ import '../styles/styles.css';
 import MobileMenu from './modules/MobileMenu';
 import RevealOnScroll from './modules/RevealOnScroll';
 import StickyHeader from './modules/StickyHeader';
+import Modal from './modules/Modal';
 
 
+
+new Modal();
 const stickyHeader = new StickyHeader();
 const mobileMenu = new MobileMenu();
 new RevealOnScroll(document.querySelectorAll('.feature-item'), 75);
